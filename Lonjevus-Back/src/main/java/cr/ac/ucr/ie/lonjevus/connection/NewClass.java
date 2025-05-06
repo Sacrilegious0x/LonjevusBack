25b4283f8d08022ef0843cc8fb61947195289cdf
Gabriel Moya <itsgamc@gmail.com>
2025-05-05 22:15:08 -0600

    Paquetes agregados

---
+++ Lonjevus-Back/src/main/java/cr/ac/ucr/ie/lonjevus/connection/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package cr.ac.ucr.ie.lonjevus.connection;
+
+/**
+ *
+ * @author User
+ */
+public class NewClass {
+
+}
