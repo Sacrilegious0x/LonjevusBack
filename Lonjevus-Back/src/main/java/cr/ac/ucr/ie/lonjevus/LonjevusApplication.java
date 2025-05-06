6da37dbfbea48841b1f9cb20956431cd219a4a55
Gabriel Moya <itsgamc@gmail.com>
2025-05-05 21:23:21 -0600

    Agregado proyecto Springboot

---
+++ Lonjevus-Back/src/main/java/cr/ac/ucr/ie/lonjevus/LonjevusApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.ie.lonjevus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonjevusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonjevusApplication.class, args);
+	}
+
+}
