6c7d02bb48ed7dc81165c6b9603945e4d98c3128
Gabriel Moya <itsgamc@gmail.com>
2025-05-19 10:02:38 -0600

    cambios controller

--- LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/Controller/ScheduleController.java
+++ LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/Controller/ScheduleController.java
@@ -8,7 +8,6 @@ package cr.ac.ucr.ie.Lonjevus.Controller;
 import cr.ac.ucr.ie.Lonjevus.domain.Schedule;
 import cr.ac.ucr.ie.Lonjevus.service.ScheduleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
