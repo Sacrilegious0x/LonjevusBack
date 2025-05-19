0a7e86c9fc2b5b907ef8e497f178ab96dfb0966e
Gabriel Moya <itsgamc@gmail.com>
2025-05-19 09:54:11 -0600

    resolucion conflictos

--- LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/service/LocalStorageService.java
+++ LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/service/LocalStorageService.java
@@ -1,36 +1,13 @@
-<<<<<<< HEAD
-
-package cr.ac.ucr.ie.Lonjevus.service;
-
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-=======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package cr.ac.ucr.ie.Lonjevus.service;
 
->>>>>>> gabriel
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-<<<<<<< HEAD
-
-
-@Service
-public class LocalStorageService {
-    
-    private final String uploadDir = "C:/Users/Usuario/Desktop/UCR MEDIACION 2025 PRIMER CICLO/Lenguajes para aplicaciones comerciales/ProyectoLenguajes BACK/proyecto-lonjevus-back/LonjevusBack/uploads/photos/suppliers/";
-
-
-    public String save(MultipartFile file) {
-        try {
-            // Crear carpeta si no existe
-            Path uploadPath = Paths.get(uploadDir);
-=======
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -45,7 +22,6 @@ public class LocalStorageService {
     try {
             // Crear carpeta si no existe
             Path uploadPath = Paths.get(uploadDirA);
->>>>>>> gabriel
             if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             }
@@ -57,9 +33,6 @@ public class LocalStorageService {
             // Guardar archivo
             Files.copy(file.getInputStream(), filePath);
 
-<<<<<<< HEAD
-             return "photos/suppliers/" + fileName;// se muestra en la web
-=======
              return "photos/admin/" + fileName;// se muestra en la web
         } catch (IOException e) {
             throw new RuntimeException("Error al guardar archivo", e);
@@ -84,7 +57,6 @@ public class LocalStorageService {
             Files.copy(file.getInputStream(), filePath);
 
              return "photos/caregiver/" + fileName;// se muestra en la web
->>>>>>> gabriel
         } catch (IOException e) {
             throw new RuntimeException("Error al guardar archivo", e);
         }
