b75ae74f4cd8d7ece695d6fa2cdd74fe96954ccc
Gabriel Moya <itsgamc@gmail.com>
2025-06-02 00:26:28 -0600

    variable nueva dominio visita

--- LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/domain/Visit.java
+++ LonjevusBack/src/main/java/cr/ac/ucr/ie/Lonjevus/domain/Visit.java
@@ -4,10 +4,115 @@
  */
 package cr.ac.ucr.ie.Lonjevus.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.sql.Time;
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  *
  * @author User
  */
+@Entity
+@Table (name="visit")
 public class Visit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "visitDate")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate visitDate;
+    @Column(name = "visitHour")
+    private Time visitHour;
+    @Column(name = "phoneNumber")
+    private int phoneNumber;
+     @Column(name = "email")
+    private String email;
+     @Column(name = "relationship")
+    private String relationship;
+     @Column(name = "isActive")
+     private boolean isActive;
+    @ManyToOne
+    @JoinColumn(name = "residentId")
+    private Resident resident;
+
+    public Visit() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(LocalDate visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+
+    public Resident getResident() {
+        return resident;
+    }
+
+    public void setResident(Resident resident) {
+        this.resident = resident;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+    
+    
+            
     
 }
